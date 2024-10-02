c61cbce70d908cdf9eb40cbd492f1d22b446f943
julietaaaaaaaaaaaa <julia.souza3@estudante.ifms.edu.br>
2024-10-02 10:39:02 -0400

    pe

--- src/exemplo3/index.jsx
+++ src/exemplo3/index.jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [Usuario, setUsuario] = useState([]);
+  const [usuario, setUsuario] = useState();
 
   useEffect(() => { 
 
     const buscarUsuario  = async () => {
         const resposta = await fetch('https://randomuser.me/api');
         const dados = await resposta.json();
-        setUsuario(dados);
+        setUsuario(dados.results[0]);
     }
     buscarUsuario();
   }, []);
@@ -18,12 +18,14 @@ export default function App() {
     <>
       <h1>Usuário</h1>
       <ul>
-      {Usuario.map(Usuario => (
-          <li key={Usuario.results[0].gender}>
-          
-            
+          <li>
+          <p>{usuario?.name.title}</p>
+            <p>{usuario?.name.firts}</p>
+            <p>{usuario?.name.last}</p>
+            <p>usuario?.location.street</p>
+            <p>usuario?.location.number</p>
+            <p>usuario?.location.name</p>
           </li>
-        ))}
       </ul>
     </>
   );
